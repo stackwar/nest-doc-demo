d077607d780228e807a92dd675f1ee414ca4dcb9
stackwar <mrhongdada@gmail.com>
2022-08-08 10:22:39 +0800

    feat: test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body, Param } from '@nestjs/common';
 import { execCmd } from './utils';
 
 @Controller()
 export class AppController {
   // constructor(private readonly appService: AppService) {}
   @Post('hook')
-  async hook() {
+  async hook(@Body() body, @Param() param) {
     await execCmd('git pull');
-    console.log('pull done');
+    console.log(body, param, 'pull done');
   }
 }
